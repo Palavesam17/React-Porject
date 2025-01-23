174cd9222b6fab734c1a4644c23a8cbe12505364
Palavesam17 <148433606+Palavesam17@users.noreply.github.com>
2025-01-23 13:07:01 +0530

    Delete Task-Management-app-main directory

--- Task-Management-app-main/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import tasksreducer from './slices/taskslice'
-
-export const store = configureStore({
-    reducer:{
-        tasks:tasksreducer
-    }
-})
